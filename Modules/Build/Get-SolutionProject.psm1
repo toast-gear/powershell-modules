e6ad024f01d8235e3e3b7dabbeb2147c09584560
toast-gear <15716903+toast-gear@users.noreply.github.com>
2018-01-11 21:17:48 +0000

    Renaming to follow PS standards + Removing lists

--- Modules/Build/Get-SolutionProject.psm1
+++ Modules/Build/Get-SolutionProject.psm1
@@ -6,7 +6,7 @@
 .EXAMPLE
    Get-SolutionProjects -Path status_console.sln
 #>
-function Get-SolutionProjects
+function Get-SolutionProject
 {
     [CmdletBinding()]
 	param
@@ -32,13 +32,11 @@ function Get-SolutionProjects
     {
         # This DLL comes with Visual Studio
         Add-Type -Path (Join-Path -Path ${env:ProgramFiles(x86)} -ChildPath 'Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\Microsoft.Build.dll')
-        ($SolutionList = [System.Collections.Generic.List[System.Collections.Generic.List[System.Object]]]("")).RemoveAt(0)
     }
     Process
     {
         $SolutionFile = Get-ChildItem -Path $FullName
         $Solution = [Microsoft.Build.Construction.SolutionFile]::Parse($SolutionFile.FullName)
-        ($ProjectsList = [System.Collections.Generic.List[System.Object]]("")).RemoveAt(0)
         $Solution.ProjectsInOrder |
         # This filters out web site projects that are part of the solution e.g. a React website
         Where-Object { $_.ProjectType -eq 'KnownToBeMSBuildFormat'} |
@@ -47,16 +45,16 @@ function Get-SolutionProjects
                 $IsWebProject = (Select-String -Pattern "<UseIISExpress>.+</UseIISExpress>" -Path $_.AbsolutePath) -ne $null
                 $Object = New-Object System.Object
                 $Object | Add-Member -MemberType NoteProperty -Name AttachedSolutionFullName -Value $SolutionFile.FullName
+                $Object | Add-Member -MemberType NoteProperty -Name AttachedSolutionName -Value $SolutionFile.BaseName
                 $Object | Add-Member -MemberType NoteProperty -Name IsWebProject -Value $IsWebProject
                 $Object | Add-Member -MemberType NoteProperty -Name FullName -Value $_.AbsolutePath
                 $Object | Add-Member -MemberType NoteProperty -Name DirectoryFullName -Value "$(Split-Path -Path $_.AbsolutePath -Resolve)"
                 $Object | Add-Member -MemberType NoteProperty -Name Name -Value $_.ProjectName
-                $ProjectsList.Add($Object)
+                $Object
             }
-        $SolutionList.Add($ProjectsList)
     }
     End
     {
-        return $SolutionList
+        return
     }
 }
