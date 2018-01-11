203297f0edd98199607b86f0fabe5d7be522a376
toast-gear <15716903+toast-gear@users.noreply.github.com>
2018-01-11 21:19:55 +0000

    Adding base module to clean projects
    A normal msbuild clean leaves behind the folders

---
+++ Modules/Build/Clean-Project.psm1
@@ -0,0 +1,45 @@
+﻿<#
+.Synopsis
+   Module for getting a list of all projects attached to a specific .NET solution files
+.EXAMPLE
+   (Get-ChildItem -Path .\* -Recurse -Include *.csproj) | Clean-Project 
+.EXAMPLE
+   Clean-Project -FullName someProject.csproj
+#>
+function Clean-Project
+{
+    [CmdletBinding()]
+	param
+	(
+        [Parameter(ValueFromPipelineByPropertyName,
+                   ParameterSetName='Path to project file')]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript( {
+            if((Test-Path -Path $_ ))
+            {
+                return $true
+            }
+            else
+            {
+                Throw "$_ failed a Test-Path check, ensure the specific project file exists"
+            }
+        })]
+        [String]
+		$FullName
+	)
+
+    Begin
+    {
+        Write-Output 'Cleaning supplied projects for a fresh build'
+    }
+    Process
+    {
+        $ProjectFile = Get-ChildItem -Path $FullName
+        Remove-Item "$($ProjectFile.Directory.FullName)\bin" -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
+        Remove-Item "$($ProjectFile.Directory.FullName)\obj" -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
+    }
+    End
+    {
+        return
+    }
+}
